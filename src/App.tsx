086b966fc84b34dd7991aceca1e6b8ff24c5f7e9
kicikhaluk <f.halukkicik@outlook.com>
2024-10-28 21:39:17 +0300

    build(initial-setup): initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate
+        cupiditate excepturi non odio veritatis voluptas cumque voluptatibus
+        rerum molestiae magnam.
+      </h1>
+    </div>
+  );
+}
+
+export default App;
